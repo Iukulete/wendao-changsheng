731d4920c6d9319e0b69c5f769a89b725a44959f
chenwh <1173519104@qq.com>
2026-06-30 21:46:32 +0800

    Target Gemma 4 inner LoRA layers

--- ai_engine/lora/train_gemma4_lora.py
+++ ai_engine/lora/train_gemma4_lora.py
@@ -103,7 +103,13 @@ def parse_args():
     parser.add_argument("--lora-r", type=int, default=16)
     parser.add_argument("--lora-alpha", type=int, default=32)
     parser.add_argument("--lora-dropout", type=float, default=0.05)
-    parser.add_argument("--target-modules", default="q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj")
+    parser.add_argument(
+        "--target-modules",
+        default=(
+            "q_proj.linear,k_proj.linear,v_proj.linear,o_proj.linear,"
+            "gate_proj.linear,up_proj.linear,down_proj.linear"
+        ),
+    )
     parser.add_argument("--load-in-4bit", action="store_true")
     parser.add_argument("--skip-kbit-prepare", action="store_true")
     parser.add_argument("--log-every", type=int, default=10)
